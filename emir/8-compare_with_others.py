24ccbfa2bfd3e7a70daea33fb546214f934f38b9
efecini-plentific <efe.cini@plentific.com>
2024-02-22 13:27:50 +0300

    emir

--- emir/8-compare_with_others.py
+++ emir/8-compare_with_others.py
@@ -1,15 +1,36 @@
-finals_file_path = "finals.txt"
-allseed_file_path = "allseed.txt"
-
-with open(finals_file_path, "r", encoding="utf-8") as finals_file:
+with open('finals.txt', 'r' , encoding='utf-8') as finals_file:
     finals_lines = finals_file.readlines()
 
-with open(allseed_file_path, "r", encoding="utf-8") as allseed_file:
+with open('allseed.txt', 'r', encoding='utf-8') as allseed_file:
     allseed_lines = allseed_file.readlines()
 
-filtered_finals_lines = [line for line in finals_lines if not any(line.startswith(seed[:4]) for seed in allseed_lines)]
+newf = []
+for line in finals_lines:
+    line = line.strip()
+    keep_line = True
 
-with open(finals_file_path, "w", encoding="utf-8") as updated_finals_file:
-    updated_finals_file.writelines(filtered_finals_lines)
+    for allseed_line in allseed_lines:
+        allseed_line = allseed_line.strip()
 
-print("Process completed.")
+        if line == allseed_line:
+            keep_line = False
+            break
+
+        if line[:4] == allseed_line[:4]:
+            keep_line = False
+            break
+
+        if len(line) == 3 and len(allseed_line) >= 4 and line == allseed_line[:3]:
+            keep_line = True
+            break
+
+        if len(allseed_line) < 3:
+            continue
+
+    if keep_line:
+        newf.append(line)
+
+with open('finals.txt', 'w') as finals_file:
+    finals_file.write('\n'.join(newf))
+
+print("Keep it cool!")
